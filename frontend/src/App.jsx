e20b2a0e7f6cc338a9c30f4bb34d98f3d8c4c339
Insha Khan <khaninsha0001@gmail.com>
2025-08-13 00:03:14 +0530

    setup project frontend & backend, install imp dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Nutri Genie</h1>
+    </div>
+  )
+}
+
+export default App
